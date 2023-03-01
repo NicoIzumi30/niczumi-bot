d90b08aa55c78030b0cc2fa3e5ad25baf0796522
Nico Izumi <99137032+NicoIzumi30@users.noreply.github.com>
2023-03-01 09:47:21 +0700

    Update config.js
    update api

--- config.js
+++ config.js
@@ -1,4 +1,4 @@
 module.exports = {
-    API_KEY_RM_BG: "YOUR_API_KEY",
-    API_KEY_OPEN_AI: "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"
+    API_KEY_RM_BG: "qfVm7NzHBpULiA46hXy6mEdf",
+    API_KEY_OPEN_AI: "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"
 }
